09bd0971ebe35670106064d88927b8848cbcf8ad
617998 <sinchuri.617998@swl.k12.oh.us>
2024-10-15 08:43:17 -0400

    ini

--- src/LoopsAsCounters.java
+++ src/LoopsAsCounters.java
@@ -1,5 +1,31 @@
-public class Main {
+import java.util.Scanner;
+
+public class LoopsAsCounters {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        //counts up by 1 from 0 to 30 and prints it
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+
+        //counts down by 1 from 30 to 0
+        System.out.println();
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+
+        //counts up by 3 from 0 to 18
+        System.out.println();
+        for (int i = 0; i <= 18; i++) {
+            System.out.print(i + " ");
+            i += 2;
+        }
+
+        //counts down by 2 from 10 to 0
+        System.out.println();
+        for (int i = 10; i >= 0; i--) {
+            System.out.print(i + " ");
+            i -= 1;
+        }
     }
 }
